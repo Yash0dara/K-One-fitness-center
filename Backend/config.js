a2245a31d90392fcfe2bb6b8c71a2bafe0d57cd3
yashodara05 <154584457+Yash0dara@users.noreply.github.com>
2024-07-01 19:09:30 +0530

    backend
    backend of the website

---
+++ Backend/config.js
@@ -0,0 +1,9 @@
+module.exports ={
+    JWT_SECRET:"7PPFAVQaXfRTME6eF++SgjGmDUBzZkNEIukqn6ycBMA=",
+    EMAIL:"paraeke3659@gmail.com",
+    PASSWORD:"pwkv fezx egtz wmhj"
+    // ATLAS_URI:"mongodb+srv://Product-Manager:2002313Uk@librarr-database-cluste.ccsxsmt.mongodb.net/"
+}
+
+
+// //mongodb+srv://admin:admin123@login.xhljmir.mongodb.net/?retryWrites=true&w=majority&appName=login
